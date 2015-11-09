775d518aeec5ba82b593ad221a3bbf54fd101de2
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-09 02:37:44 +0000

    Add show and add error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,17 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect # Use this line to view params
     @article = Article.new(article_params)
-    @article.save
-    redirect_to article_path(@article)
+
+    if @article.save
+      flash[:notice] = 'Article was successfully created.'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
