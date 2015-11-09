e33c51f1f6446b32151216751dd6e1c6449cc57a
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-09 06:05:31 +0000

    Add editing of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -16,8 +24,15 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
+  def update
     @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      flash[:notice] = 'Article was successfully updated.'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   private
