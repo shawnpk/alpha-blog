a86e43d10b8ad30fd7776a0e3cbd2ed9621fe266
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-10 21:57:24 +0000

    Add user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -14,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def create
@@ -29,7 +30,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
 
     if @user.update(user_params)
       flash[:success] = 'Your account was updated successfully'
@@ -43,5 +43,9 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
