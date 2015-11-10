616eb2b6a7ac10a14efd6a876a7521df9a7e641f
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-10 23:37:47 +0000

    Complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -44,8 +45,15 @@ class UsersController < ApplicationController
       params.require(:user).permit(:username, :email, :password)
     end
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:danger] = 'You can only edit your own account'
+        redirect_to root_path
+      end
+    end
 
 end
