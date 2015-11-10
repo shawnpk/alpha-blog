4b871fbe0e81c7e3b30bf285fda0935d10e6fac1
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-10 16:16:36 +0000

    Add edit user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+
+
   def new
     @user = User.new
   end
@@ -12,6 +14,21 @@ class UsersController < ApplicationController
       redirect_to articles_path
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:success] = 'Your account was updated successfully'
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
 
