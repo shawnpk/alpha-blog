dca27378520d6f227b21151d24eb43fe5ea6033d
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-11 17:56:58 +0000

    Create integration test for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,30 @@
 class CategoriesController < ApplicationController
 
   def index
-
-  end
-  
-  def new
-
+    @categories = Category.all
   end
 
   def show
 
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = 'Your category has been successfully created'
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 
 end
